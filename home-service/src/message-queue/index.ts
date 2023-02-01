453005a3b9801b0773c1232d54bef5cbf756926e
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-02-02 02:59:16 +0600

    error handled when worker throws error

--- home-service/src/message-queue/index.ts
+++ home-service/src/message-queue/index.ts
@@ -3,27 +3,34 @@ import { Job, MessageQueue } from "../utils/constants";
 import userService from "../modules/user/services";
 
 export const worker_01 = new Worker(MessageQueue.AUTH_HOME, async (job) => {
-  console.log("processed job:", job.name, job.data);
+  try {
+    console.log("processed job:", job.name, job.data);
+    if (job.name === Job.UserCreated) {
+      const {
+        _id: user_id,
+        name,
+        email,
+        phone,
+        birthdate,
+        gender,
+        profile_photo,
+      } = job.data;
 
-  if (job.name === Job.UserCreated) {
-    const {
-      id: user_id,
-      name,
-      email,
-      phone,
-      birthdate,
-      gender,
-      profile_photo,
-    } = job.data;
-
-    userService.createUser({
-      user_id,
-      name,
-      email,
-      phone,
-      birthdate,
-      gender,
-      profile_photo,
-    });
+      // THINK:-
+      // Without 'await' keyword, error don't gets caught in catch block, server dies..
+      // So, i can't make the worker free immediately.. then what happens if the worker becomes too busy?
+      await userService.createUser({
+        user_id,
+        name,
+        email,
+        phone,
+        birthdate,
+        gender,
+        profile_photo,
+      });
+    }
+  } catch (err: any) {
+    console.error(`# Error occurred while processing job <${job.name}>...`);
+    console.error(err.stack);
   }
 });
