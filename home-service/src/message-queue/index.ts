3fc325e28103b4f24ce534927dbe74abdd496290
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-02-02 02:44:36 +0600

    user added in home db when user added in auth db using bullmq

--- home-service/src/message-queue/index.ts
+++ home-service/src/message-queue/index.ts
@@ -1,6 +1,29 @@
 import { Worker } from "bullmq";
-import { MessageQueue } from "../utils/constants";
+import { Job, MessageQueue } from "../utils/constants";
+import userService from "../modules/user/services";
 
 export const worker_01 = new Worker(MessageQueue.AUTH_HOME, async (job) => {
   console.log("processed job:", job.name, job.data);
+
+  if (job.name === Job.UserCreated) {
+    const {
+      id: user_id,
+      name,
+      email,
+      phone,
+      birthdate,
+      gender,
+      profile_photo,
+    } = job.data;
+
+    userService.createUser({
+      user_id,
+      name,
+      email,
+      phone,
+      birthdate,
+      gender,
+      profile_photo,
+    });
+  }
 });
