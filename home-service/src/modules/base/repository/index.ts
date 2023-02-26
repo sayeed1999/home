18ade250892b70644c6c44866a705b666f054ac7
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-02-26 23:25:33 +0600

    generic repository pattern used to supply common crud operations for all db collections in home service

--- home-service/src/modules/base/repository/index.ts
+++ home-service/src/modules/base/repository/index.ts
@@ -1,5 +1,59 @@
+import { Model, Document } from "mongoose";
 import { catchErrors } from "../../../api/middlewares";
 
 /**
  * This is the generic repository that all repositories will inherit from common CRUD methods!
  */
+
+export interface IBaseRepository<T extends Document> {
+  create(data: Partial<T>): Promise<T>;
+  findById(id: string): Promise<T | null>;
+  findAll(): Promise<T[]>;
+  updateById(id: string, data: Partial<T>): Promise<T | null>;
+  update(filter: any, data: Partial<T>): Promise<T | null>;
+  deleteById(id: string): Promise<T | null>;
+  delete(filter: any): Promise<T | null>;
+}
+
+export default class BaseRepository<T extends Document>
+  implements IBaseRepository<T>
+{
+  private model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this.model = model;
+  }
+
+  async create(item: Partial<T>): Promise<T> {
+    const data = await this.model.create(item);
+    return data;
+  }
+
+  async findById(id: string): Promise<T | null> {
+    const data = await this.model.findById(id);
+    return data;
+  }
+
+  async findAll(): Promise<T[]> {
+    const data = await this.model.find();
+    return data;
+  }
+
+  async updateById(id: string, item: Partial<T>): Promise<T | null> {
+    const data = await this.model.findByIdAndUpdate(id, item, { new: true });
+    return data;
+  }
+
+  async update(filter: any = {}, item: Partial<T>): Promise<T | null> {
+    const data = await this.model.findOneAndUpdate(filter, item, { new: true });
+    return data;
+  }
+
+  async deleteById(id: string): Promise<T | null> {
+    return await this.model.findByIdAndDelete(id);
+  }
+
+  async delete(filter: any = {}): Promise<T | null> {
+    return await this.model.findOneAndDelete(filter);
+  }
+}
