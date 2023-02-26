e2bb3afa9e238fc48452db8f9b61dac75edd443d
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-02-27 01:08:08 +0600

    UserService inherited from BaseService to reuse generic service methods

--- home-service/src/modules/user/controllers/rest.ts
+++ home-service/src/modules/user/controllers/rest.ts
@@ -2,34 +2,34 @@ import { catchErrors } from "../../../api/middlewares";
 import userService from "../services";
 
 export const createUser = catchErrors(async (req: any, res: any, next: any) => {
-  const data = await userService.createUser(req.body);
+  const data = await userService.create(req.body);
   res.status(201).json({ message: "Created successfully", data });
 });
 
 export const getAllUsers = catchErrors(
   async (req: any, res: any, next: any) => {
-    const data = await userService.getAllUsers();
+    const data = await userService.findAll();
     res.status(200).json({ data });
   }
 );
 
 export const getSingleUser = catchErrors(
   async (req: any, res: any, next: any) => {
-    const data = await userService.getUserById(req.params.id);
+    const data = await userService.findById(req.params.id);
     res.status(200).json({ data });
   }
 );
 
 export const updateUserById = catchErrors(
   async (req: any, res: any, next: any) => {
-    const data = await userService.updateUserById(req.params.id, req.body);
+    const data = await userService.updateById(req.params.id, req.body);
     res.status(200).json({ data });
   }
 );
 
 export const deleteUserById = catchErrors(
   async (req: any, res: any, next: any) => {
-    const data = await userService.deleteUserById(req.params.id);
+    const data = await userService.deleteById(req.params.id);
     res.status(200).json({ data });
   }
 );
--- home-service/src/modules/user/services/index.ts
+++ home-service/src/modules/user/services/index.ts
@@ -1,49 +1,14 @@
+import { IUser } from "../../../models/user.model";
 import CustomError from "../../../utils/errors/custom-error";
+import BaseService, { IBaseService } from "../../base/services";
 import userRepository from "../repository";
 
-const createUser = async (body: any) => {
-  const post = await userRepository.create(body);
-  return post;
-};
+export interface IUserService extends IBaseService<IUser> {}
 
-const getAllUsers = async () => {
-  const post = await userRepository.findAll();
-  return post;
-};
+class UserService extends BaseService<IUser> implements IUserService {
+  constructor() {
+    super(userRepository);
+  }
+}
 
-const getUserById = async (id: any) => {
-  const post = await userRepository.findById(id);
-  return post;
-};
-
-const updateUserById = async (id: any, body: any) => {
-  const post = await userRepository.updateById(id, body);
-  return post;
-};
-
-const updateUser = async (filter: any = {}, body: any) => {
-  const post = await userRepository.update(filter, body);
-  return post;
-};
-
-const deleteUserById = async (id: any) => {
-  const post = await userRepository.deleteById(id);
-  return post;
-};
-
-const deleteUser = async (filter: any) => {
-  if (!filter)
-    throw new CustomError("cannot delete user without filter specified", 400);
-  const post = await userRepository.delete(filter);
-  return post;
-};
-
-export default {
-  createUser,
-  getAllUsers,
-  getUserById,
-  updateUserById,
-  updateUser,
-  deleteUserById,
-  deleteUser,
-};
+export default new UserService();
