105c6d2a105d598a819b6aaa5a39034ae4cbb9f5
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-02-27 00:04:57 +0600

    generic BaseService made to supply common functionalities to all services

---
+++ home-service/src/modules/base/services/index.ts
@@ -0,0 +1,55 @@
+import { Document } from "mongoose";
+import { IBaseRepository } from "../repository";
+
+export interface IBaseService<T extends Document> {
+  create(data: Partial<T>): Promise<T>;
+  findById(id: string): Promise<T | null>;
+  findAll(filter: any): Promise<T[]>;
+  updateById(id: string, data: Partial<T>): Promise<T | null>;
+  update(filter: any, data: Partial<T>): Promise<T | null>;
+  deleteById(id: string): Promise<T | null>;
+  delete(filter: any): Promise<T | null>;
+}
+
+export default class BaseService<T extends Document>
+  implements IBaseService<T>
+{
+  private repository: IBaseRepository<T>;
+
+  constructor(repository: IBaseRepository<T>) {
+    this.repository = repository;
+  }
+
+  async create(item: Partial<T>): Promise<T> {
+    const data = await this.repository.create(item);
+    return data;
+  }
+
+  async findById(id: string): Promise<T | null> {
+    const data = await this.repository.findById(id);
+    return data;
+  }
+
+  async findAll(filter: any = {}): Promise<T[]> {
+    const data = await this.repository.findAll(filter);
+    return data;
+  }
+
+  async updateById(id: string, item: Partial<T>): Promise<T | null> {
+    const data = await this.repository.updateById(id, item);
+    return data;
+  }
+
+  async update(filter: any = {}, item: Partial<T>): Promise<T | null> {
+    const data = await this.repository.update(filter, item);
+    return data;
+  }
+
+  async deleteById(id: string): Promise<T | null> {
+    return await this.repository.deleteById(id);
+  }
+
+  async delete(filter: any = {}): Promise<T | null> {
+    return await this.repository.delete(filter);
+  }
+}
